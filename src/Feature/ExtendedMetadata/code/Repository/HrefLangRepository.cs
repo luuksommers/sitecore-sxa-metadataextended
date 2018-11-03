57486bd8810d9488619e0baba01dd66f5bce509b
Luuk Sommers <luuk.sommers@gmail.com>
2018-11-03 23:26:45 +0100

    Remove datasource dependency for HrefLangRepository

--- src/Feature/ExtendedMetadata/code/Repository/HrefLangRepository.cs
+++ src/Feature/ExtendedMetadata/code/Repository/HrefLangRepository.cs
@@ -1,6 +1,10 @@
-﻿using Sitecore.Data.Fields;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.Data.Fields;
+using Sitecore.Data.Items;
+using Sitecore.DependencyInjection;
 using Sitecore.Globalization;
 using Sitecore.Links;
+using Sitecore.XA.Foundation.Multisite;
 using Sitecore.XA.Foundation.Mvc.Repositories.Base;
 using Sitecore.XA.Foundation.RenderingVariants.Repositories;
 using SXA.Feature.MetadataExtended.Models;
@@ -14,13 +18,17 @@ namespace SXA.Feature.MetadataExtended.Repositories
             var model = new HrefLangRenderingModel();
             FillBaseProperties(model);
 
-            var supportedLanguagesField = new MultilistField(model.Item.Fields["SupportedLanguages"]);
-            var supportedLanguages = supportedLanguagesField.GetItems();
             var item = Sitecore.Context.Item;
+            var settings = GetSettingsItem(item).Axes.GetChild("Languages");
+            var supportedLanguagesField = new MultilistField(settings.Fields["SupportedLanguages"]);
+            var supportedLanguages = supportedLanguagesField.GetItems();
             
             foreach (var lang in supportedLanguages)
             {
+                // We have to get the language item for the right display-name
                 var languageItem = item.Database.GetItem(item.ID, Language.Parse(lang.Name));
+
+                // And use the Language switcher to get the right language in the url
                 using (new LanguageSwitcher(lang.Name))
                 {
                     var url = LinkManager.GetItemUrl(languageItem);
@@ -29,6 +37,11 @@ namespace SXA.Feature.MetadataExtended.Repositories
             }
 
             return model;
+        }
+
+        protected virtual Item GetSettingsItem(Item contextItem)
+        {
+            return ServiceLocator.ServiceProvider.GetService<IMultisiteContext>().GetSettingsItem(contextItem);
         }
     }
 }
